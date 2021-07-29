4727a55a0ce0b5a7c859fbfce5c0f3871f702c9f
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-29 14:12:18 -0300

    style(bikewise project): prettier first execution

--- helio-bikewise/pages/_app.tsx
+++ helio-bikewise/pages/_app.tsx
@@ -3,15 +3,17 @@ import PropTypes, { ReactComponentLike } from "prop-types";
 import React from "react";
 
 interface MyAppArgs {
-	Component: ReactComponentLike | undefined;
-	pageProps: unknown;
+  Component: ReactComponentLike | undefined;
+  pageProps: unknown;
 }
 
-const MyApp: React.FC<MyAppArgs> = ({ Component, pageProps }): JSX.Element => <Component {...pageProps} />;
+const MyApp: React.FC<MyAppArgs> = ({ Component, pageProps }): JSX.Element => (
+  <Component {...pageProps} />
+);
 
 MyApp.propTypes = {
-	Component: PropTypes.elementType,
-	pageProps: PropTypes.object
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object,
 };
 
 export default MyApp;
